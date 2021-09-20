12004be84fd18a6e3d69b99966b2631af8cba50d
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-09-20 10:10:35 +0200

    Add files via upload

--- ejercicios/ej6.php
+++ ejercicios/ej6.php
@@ -1,17 +1,31 @@
 <?php
-$nArray[];//Acabar
+$x=0;
+
 $nombre="";
+$nArray[$x]=$nombre;
+$edad=0;
+
 
 while($nombre!="fin"){
 echo "Nombre?";
 fscanf(STDIN, "%s\n", $nombre);
-$nArray[]=$nombre;
-//probar un for para guardar el nombre dentro del array
+for($i=0;$i=$x;$i++){
+    $nArray[$i]=$nombre;
+}
+
 if($nombre!="fin"){
 echo "Edad de $nombre?";
 fscanf(STDIN, "%d\n", $edad);
 $eArray[$nombre]=$edad;
+for($i=0;$i=$x;$i++){
+    $eArray[$i]=$edad;
 }
+}
+if($nombre=="fin"){
+   
+    echo "$nArray[$x]($eArray[$nombre]), ";
+    
+    }
 }
 
 ?>
