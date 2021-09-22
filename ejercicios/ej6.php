e223fe0700f08100f8a75764d0c79a69cc579e14
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-09-22 16:39:33 +0200

    Add files via upload

--- ejercicios/ej6.php
+++ ejercicios/ej6.php
@@ -1,3 +1,4 @@
+   
 <?php
 echo "Nombre?";
 fscanf(STDIN, "%s\n", $nombre);
@@ -12,8 +13,29 @@ fscanf(STDIN, "%s\n", $nombre);
 foreach($nArray as $nombre=>$edad){
    
     echo "$nombre($edad), ";
-    
+   
+    }
+    echo "\n";
+    echo "Quieres ordenar la info por nombre?";
+    fscanf(STDIN, "%s\n", $ordenarN);
+   
+    if ($ordenarN=="si"){
+        ksort($nArray);
+        foreach($nArray as $nombre=>$edad){
+       
+        echo "$nombre($edad), ";
+    }
+    }
+    echo "\n";
+    echo "Quieres ordenar la info por edad?";
+    fscanf(STDIN, "%s\n", $ordenarE);
+    if ($ordenarE=="si"){
+        asort($nArray);
+        foreach($nArray as $nombre=>$edad){
+            
+            echo "$nombre($edad), ";
+        }
     }
 
-
+   
 ?>
