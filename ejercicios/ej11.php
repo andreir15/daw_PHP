8b93fb0641ee1c43b9458f2b3fb2b65d4917de6a
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-09-22 17:19:11 +0200

    Add files via upload

--- ejercicios/ej11.php
+++ ejercicios/ej11.php
@@ -1,14 +1,14 @@
+   
 <?php
 $cadena = "Alberto:Garay:913334455//Ana:Garcia:914445566";
-$cArray=["Nombre","Apellido","Teléfono","Nombre","Apellido","Teléfono"];
-$i=0;
 
 $cArray=explode("//",$cadena);
 foreach($cArray as $palabra){
-    $x=explode(":",$cadena);
-    echo "$x[$i]: $palabra\n";
-    $i++;
-  
+    $x=explode(":",$palabra);
+    echo "Nombre:",$x[0]."\n";
+    echo "Apellido:",$x[1]."\n";
+    echo "Telefono:",$x[2]."\n";
+    echo "--------------------------------------------\n";
 }
 
 
