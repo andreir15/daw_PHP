853d17a7c1b58de3852d1a01e07433914cfdaa13
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-09-30 17:25:00 +0200

    Add files via upload

--- t2/ejercicios/ejercicioRadioButton.php
+++ t2/ejercicios/ejercicioRadioButton.php
@@ -1,7 +1,9 @@
 <form action="ejercicioRadioButton2.php">
 Escoge un numero<br/>
-<input type="radio" name="num" value="uno"/>1<br/>
-<input type="radio" name="num" value="dos"/>2<br/>
-<input type="radio" name="num" value="tres"/>3<br/>
+<input type="radio" name="num" value="1"/><label for="uno" checked>1</label><br/>
+<input type="radio" name="num" value="2"/><label for="dos">2</label><br/>
+<input type="radio" name="num" value="3"/><label for="tres">3</label><br/>
+<input type="radio" name="num" value="4"/><label for="cuatro">4</label><br/>
+<input type="radio" name="num" value="5"/><label for="cinco">5</label><br/>
 <input type="submit"/>
 </form>
