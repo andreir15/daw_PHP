d1ca498d73f8254c303a87b5e6d9b5f20de26a73
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-09-30 17:22:30 +0200

    Delete ejercicioRadioButtonClase.php

--- t2/ejercicios/ejercicioRadioButtonClase.php
+++
@@ -1,9 +0,0 @@
-<form action="ejercicioRadioButton2.php">
-Escoge un numero<br/>
-<input type="radio" name="num" value="1"/><label for="uno" checked>1</label><br/>
-<input type="radio" name="num" value="2"/><label for="dos">2</label><br/>
-<input type="radio" name="num" value="3"/><label for="tres">3</label><br/>
-<input type="radio" name="num" value="4"/><label for="cuatro">4</label><br/>
-<input type="radio" name="num" value="5"/><label for="cinco">5</label><br/>
-<input type="submit"/>
-</form>
