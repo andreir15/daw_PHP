d5291bbefba0a6feefdb275da95db654abc8bd4a
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-09-30 12:26:37 +0200

    Add files via upload

--- t2/ejercicios/ejercicioRadioButton2.php
+++ t2/ejercicios/ejercicioRadioButton2.php
@@ -1,12 +1,23 @@
-<input id="ckuno" type="checkbox" name="num[]" value="uno"/>
-<label for="ckuno">1</label><br/>
-<input id="ckdos" type="checkbox" name="num[]" value="dos"/>
-<label for="ckdos">2</label><br/>
-<input id="cktres" type="checkbox" name="num[]" value="tres"/>
-<label id="cktres">3</label><br/>
+<h1>Escoge un checkbox</h1>
+
 <?php
-$numBD=["uno"=>"ckuno","dos"=>"ckdos","tres"=>"cktres"];
-$numIdx=numero(isset($_GET["uno"])?$_GET["uno"]:"nose");
-$numero= array_key_exists($numIdx, $numBD)?$numBD[$numIdx]:"desconocido";
+$checkbox=isset($_GET["uno"])?$_GET["uno"]:3;
+$checkbox=($checkbox>5)? 5 :$checkbox;
+$num=["uno","dos","tres","cuatro","cinco"];
+for($i=0;$i<$checkbox;$i++){
+    $envio=$i+1;
+    echo <<<HTML
+<form action="ejercicioRadioButton3.php>
+HTML;
+  echo <<<HTML
+<input id="id-$i" type="checkbox" name="eleccion[]" value="$envio"/>
+<label for="id-$i">$num[$i]</label><br/>
+HTML; 
+echo <<<HTML
+<input type="submit"/>
+</form>
+HTML;
+}
+
 
 ?>
