36c20576202d7964cd2fdb5dc7c55c96cbb3799a
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-10-04 10:01:55 +0200

    Delete dos.php

--- t2/ejemplos/dos.php
+++
@@ -1,7 +0,0 @@
-<h1>DATOS</h1>
-<?php 
-$nombre=isset ($_GET["nombre"])?$_GET["nombre"]: "John";
-$apellido=isset ($_GET["apellido"])?$_GET["apellido"]: "Doe";
-echo "<h3>Hola $nombre $apellido</h3>";
-?><!--Para usar el $_POST hay que poner method="post"
-porque el metodo por defecto es get -->
