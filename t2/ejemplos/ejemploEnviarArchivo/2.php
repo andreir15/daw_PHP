8842a6216d4255ef59b3aa7be1fb4f5b4e2f362e
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-10-04 10:13:11 +0200

    Add files via upload

--- t2/ejemplos/ejemploEnviarArchivo/2.php
+++ t2/ejemplos/ejemploEnviarArchivo/2.php
@@ -1,2 +1,8 @@
 <?php 
-echo "<h1>RECIBIDO</h1>"?>
+$nombre=$_POST["nombre"];
+$nombreArchivo=$_FILES["foto"]["name"];
+echo "<h1>Hola $nombre. RECIBIDO el archivo $nombreArchivo</h1>";
+$carpeta="/tmp/";
+copy  ($_FILES ["foto"]["tmp_name"],$carpeta. $nombreArchivo);
+echo "Archivo copiado";
+?>
