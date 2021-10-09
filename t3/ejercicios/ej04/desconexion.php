417a62f25e28cbb3f0b9f4e9cea596e1e0c08325
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-10-09 19:37:57 +0200

    Add files via upload

--- t3/ejercicios/ej04/desconexion.php
+++ t3/ejercicios/ej04/desconexion.php
@@ -1,5 +1,8 @@
 <h1>Tratamiento de cookies</h1>
-<p>Primera ejecucion. Usted no esta conectado.</p>
+<?php 
+$usuario=$_GET["usuario"];
+echo "<p>Adios $usuario. Usted no esta conectado.</p>";
+?>
 <form action="verificar.php">
 <label for="usuario">Usuario</label><input type="text" id="usuario" name="usuario"/>
 <label for="clave">Clave</label><input type="password" id="clave" name="clave"/>
--- t3/ejercicios/ej04/verificar.php
+++ t3/ejercicios/ej04/verificar.php
@@ -1,17 +1,12 @@
-<a href="index.php">Volver</a>
 <?php
 $usuario=isset($_GET["usuario"])?$_GET["usuario"]:null;
 $clave=isset($_GET["clave"])?$_GET["clave"]:null;
-
+$visitas=0;
 if ($usuario!=null && $clave != null) {
-    /*$rutaBase = pathinfo($_SERVER["REQUEST_URI"])["dirname"];
-    $ruta = ($nivel==0?
-        $rutaBase."/":
-        ($nivel==1?$rutaBase."/nivel1/":
-            $rutaBase."/nivel1/nivel2/"));*/
-    setcookie ( $usuario, $clave );
-echo "<p>Conectado como $usuario</p>";
+    setcookie ( $usuario, $clave,$visitas++ );
+        echo "<p>Conectado como $usuario. Numero de visitas: $visitas</p>";
 }
+
 else {
     if ($usuario==null) {
         echo "El nombre no puede ser nulo<br/>";
@@ -22,4 +17,10 @@ else {
     }
     
 }
+
+
+
 ?>
+<form action="desconexion.php">
+<input type="submit" value="Desconectar"/>;
+</form>
