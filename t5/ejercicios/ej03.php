24d4b92f0e6a72e83b359731f4f2051348132810
andreir15 <90853700+andreir15@users.noreply.github.com>
2021-10-26 13:27:34 +0200

    Add files via upload

--- t5/ejercicios/ej03.php
+++ t5/ejercicios/ej03.php
@@ -1,40 +1,48 @@
+<?php session_start();?>
 Nombre producto<input type="text" id="nombre"/>
 <br/>
 Precio producto<input type="text" id="precio"/>
 <br/>
-<input type="submit" value="Crear producto"/>
+<input type="submit" value="Preparar" name="accion"/> 
+<br/>
+<input type="submit" value="Insertar" name="accion"/>
+<?php if (isset($_SESSION["preparar"])):?>
+<h3>Productos preparados</h3>
+<?php foreach ($_SESSION["preparados"] as $producto):?>
+<?php endforeach;?>
+<?php endif;?>
+<h3>Productos insertados</h3>
 <?php
 require_once '../utilBD.php';
 $bd=conectarMySQL();
+
 $nombre=isset($_GET["nombre"])?$_GET["nombre"]:null;
 $precio=isset($_GET["precio"])?$_GET["precio"]:null;
 $productos=[$nombre=>$precio];
+if($_GET["accion"]=="Insertar"){
 if($nombre!=null&&$precio!=null){
     foreach ($productos as $k){
         
-        $sql1= <<<SQL
+        $sql= <<<SQL
 insert into producto(nombre,precio)
 values('$k[0]',$k[1])
 
 SQL;
     }
     header("Location:ej02.php");
-
-        $bd->exec($sql1);
-        echo "Producto creado<br/>";
 }
-    
-    $sql= <<<SQL
+}
+$bd->exec($sql);
+echo "Producto creado<br/>";
+
+
+$sql= <<<SQL
 select nombre,precio
 form producto
 
 SQL;
-$bd->query($sql);    
-foreach ($productos as $k){
-    echo "$k[0]=$k[1]<br/>";
-}
-    
-    
+$bd->query($sql);
+
 
 
 ?>
